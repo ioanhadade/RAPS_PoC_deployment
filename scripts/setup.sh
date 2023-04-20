8ccde088c903eddd0411623b86aef1ab9331a0e6
Cloud User <azureuser@tfvm.pe0i1hag3dsu3dpaywnnijujme.bx.internal.cloudapp.net>
2023-04-20 12:29:45 +0000

    installs some prereqs

---
+++ scripts/setup.sh
@@ -0,0 +1,19 @@
+#installs libraries lke terraform and azcopy if you don't already have them
+set -x
+set -e
+
+#Install terraform
+#assumes your running rhel linux (yum) but you can swap them out
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+sudo yum -y install terraform
+terraform --version
+
+#Install Azure CLI
+yum check-update
+sudo yum install -y gcc libffi-devel python36u-devel openssl-devel
+curl -L https://aka.ms/InstallAzureCli | bash
+
+echo "now type 'az login' to complete setup"
+az login
+
