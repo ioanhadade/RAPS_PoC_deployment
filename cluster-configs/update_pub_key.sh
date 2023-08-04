f47e9615cbb0501602c4df38a42405c74dd138e8
Cloud User <azureuser@tfvm.ztmetpupgawedls3zrl2sf1woh.bx.internal.cloudapp.net>
2023-08-04 12:07:49 +0000

    nice script to update a given users pub key via commandline

---
+++ cluster-configs/update_pub_key.sh
@@ -0,0 +1,36 @@
+#script to update a CC users pub key via commandline
+#takes a username, a public key file and a cyclecloud host IP
+#NOTE currently will overwrite previously existing $output_file, shouldnt matter if this file just triggers an update on creation
+ 
+echo "usage: bash update_pub_key.sh [user to be updated] [new pub key] [cyclecloud host IP]"
+set -u #stop if no CLAs given
+
+cd "$(dirname "$0")"
+
+username=$1
+pub_key_file=$2
+cycleserver=$3
+output_file="pubkeyupdate.txt"
+output_file_loc="/opt/cycle_server/config/data"
+
+#read file
+echo "reading $2"
+pub_key=`cat $2`
+
+#generate update file
+echo "generating update file to $output_file"
+cat > $output_file <<- EOM
+AdType = "Credential"
+Name = "$username/public"
+CredentialType = "PublicKey"
+PublicKey = "$pub_key"
+EOM
+
+#send update file to cycleserver
+echo "sending $output_file to $cycleserver:$output_file_loc"
+#scp -i ../.ssh/cc_key $output_file hpc_admin@$cycleserver:$output_file_loc #doesnt work need root perms for output_file_loc
+#hacky workaround
+cat $output_file | ssh -i ../.ssh/cc_key hpc_admin@$cycleserver "sudo tee -a $output_file_loc/$output_file"
+
+#cleanup
+rm $output_file
