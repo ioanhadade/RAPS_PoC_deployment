6049a61f93e9926a5541e75249b29f7f31d2d6c9
Cloud User <azureuser@tfvm.cnojqtpezqnunhpv1tb1spnazg.bx.internal.cloudapp.net>
2023-08-11 13:29:45 +0000

    cleanup

--- scripts/deploy_cyclecloud_host.sh
+++ scripts/deploy_cyclecloud_host.sh
@@ -2,6 +2,8 @@ cd "$(dirname "$0")"/../deploy-cyclecloud
 terraform init
 pub_key=$(<../.ssh/cc_key.pub) #this doesnt even do anything unless run in root
 #export TF_VAR_cyclecloud_user_publickey=$pub_key
-terraform plan -var="cyclecloud_user_publickey=$pub_key"
+
+echo("Make sure you add you SP details to deploy-cyclecloud/terraform.tfvars")
+terraform plan -var="cyclecloud_user_publickey=$pub_key" \
 #sudo terraform apply --auto-approve -var="cyclecloud_user_publickey=$pub_key"
 terraform apply --auto-approve -var="cyclecloud_user_publickey=$pub_key"
