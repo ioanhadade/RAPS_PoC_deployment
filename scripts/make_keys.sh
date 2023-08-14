5060e8a12128fefc777ee306208cb9b54d0b2f4d
Cloud User <azureuser@tfvm.cnojqtpezqnunhpv1tb1spnazg.bx.internal.cloudapp.net>
2023-08-14 08:18:08 +0000

    fixed bug with unprotected key files

--- scripts/make_keys.sh
+++ scripts/make_keys.sh
@@ -2,5 +2,6 @@ cd "$(dirname "$0")"/..
 mkdir -p .ssh #for some reason having ssh key in ~/.ssh seems to cause problems, so put it in ~/RAPS_PoC_deployment/.ssh instead
 rm -rf .ssh/*
 ssh-keygen -t rsa -b 4096 -C "cc_key" -f $PWD/.ssh/cc_key -N "" #no password
+chmod 600 $PWD/.ssh/cc_key
 eval `ssh-agent`
 ssh-add $PWD/.ssh/cc_key
