ea3ed60af2df1c6286550a1386d56bb152c8620e
Cathal OBrien <Cathal.OBrien@ecmwf.int>
2023-08-23 13:56:53 +0200

    reads sensitive info from env file now

--- deploy-cyclecloud/terraform.tfvars
+++ deploy-cyclecloud/terraform.tfvars
@@ -1,12 +1,9 @@
 #File which will be read automatically when running "tf plan" to initalise variables
-cyclecloud_tenant_id = ""
-cyclecloud_application_id = ""
-cyclecloud_application_secret = ""
 
 cyclecloud_password = "Ecmwf_demo1"
 cyclecloud_username = "hpc_admin"
 
-cyclecloud_dns_label= "tf-cyclecloud" #optional
+cyclecloud_dns_label= "tf-cyclecloud"
 location = "eastus"
 
 prefix = "raps-poc-deployment"
--- scripts/deploy_cyclecloud_host.sh
+++ scripts/deploy_cyclecloud_host.sh
@@ -1,10 +1,16 @@
 cd "$(dirname "$0")"/../deploy-cyclecloud
 terraform init
-pub_key=$(<../.ssh/cc_key.pub) #this doesnt even do anything unless run in root
-#export TF_VAR_cyclecloud_user_publickey=$pub_key
 
-echo "Make sure you add you SP details to deploy-cyclecloud/terraform.tfvars"
-terraform plan -var="cyclecloud_user_publickey=$pub_key"
+source ../config.env #load cyclecloud details
+pub_key=$(<../.ssh/cc_key.pub)
 
-#sudo terraform apply --auto-approve -var="cyclecloud_user_publickey=$pub_key"
-terraform apply --auto-approve -var="cyclecloud_user_publickey=$pub_key"
+echo "Make sure you add you SP details to RAPS_PoC_deployment/config.env"
+terraform plan -var="cyclecloud_tenant_id=$cyclecloud_tenant_id" \
+	-var="cyclecloud_application_id=$cyclecloud_application_id" \
+	-var="cyclecloud_application_secret=$cyclecloud_application_secret" \
+	-var="cyclecloud_user_publickey=$pub_key"
+
+terraform apply --auto-approve -var="cyclecloud_tenant_id=$cyclecloud_tenant_id" \
+        -var="cyclecloud_application_id=$cyclecloud_application_id" \
+        -var="cyclecloud_application_secret=$cyclecloud_application_secret" \
+	-var="cyclecloud_user_publickey=$pub_key"
