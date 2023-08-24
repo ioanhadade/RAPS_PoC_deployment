68ffe9c50a96242f21d0491f992224d1a89bdc72
Cathal OBrien <Cathal.OBrien@ecmwf.int>
2023-08-24 16:54:41 +0200

    detect package manager and build accordingly

--- scripts/install_prereqs.sh
+++ scripts/install_prereqs.sh
@@ -1,27 +1,38 @@
 #installs libraries lke terraform and azcopy if you don't already have them
-set -x
 set -e
 
-#Install terraform
-#assumes your running rhel linux (yum) but you can swap them out
-sudo yum install -y yum-utils
-sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
-sudo yum -y install terraform
-terraform --version
+echo "detecting package manager..."
+#detect system package manager
+if brew -v; then #using mac
+	echo "using brew"
 
-#Install Azure CLI
-sudo yum check-update & true
-sudo yum install -y gcc libffi-devel python36-devel openssl-devel
-curl -L https://aka.ms/InstallAzureCli | bash
+	brew update && brew install azure-cli
+	brew install terraform
 
-echo "now type 'az login' to complete setup"
-#uncomment these lines to run as root, not needed currently tho
-#cp ~/bin/az /usr/bin #so root can find it
-#sudo az login 
+	brew install jq #needed bc mac uses a non-standard version of sed
+
+	#needed for cyclecloud on mac
+	export LDFLAGS="-L/opt/homebrew/opt/openssl@1.1/lib"
+	export CPPFLAGS="-I/opt/homebrew/opt/openssl@1.1/include"
+
+elif yum -v; then #using RHEL/centos/alma
+	echo "using yum"
+
+	#Install terraform
+	sudo yum install -y yum-utils
+	sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+	sudo yum -y install terraform
+	terraform --version
+
+	#Install Azure CLI
+	sudo yum check-update & true
+	sudo yum install -y gcc libffi-devel python36-devel openssl-devel
+	curl -L https://aka.ms/InstallAzureCli | bash
+	
+else
+	echo "You system isn\'t supported. please try install terraform and the Azure CLI yourself"
+	exit 1
+fi
+
 az login 
 
-#on mac:
-#	brew install terraform
-#	brew install jq
-#	export LDFLAGS="-L/opt/homebrew/opt/openssl@1.1/lib"
-#	export CPPFLAGS="-I/opt/homebrew/opt/openssl@1.1/include"
